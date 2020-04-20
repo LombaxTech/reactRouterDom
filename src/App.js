4420a16941c90d950506fa21dbca01ce40e195a3
Rakib Khan <rakibkhan@live.co.uk>
2020-04-21 00:13:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './Components/Nav';
+import About from './Components/About';
+import Shop from './Components/Shop';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Nav />
+                <Route path="/about" component={About} />
+                <Route path="/shop" component={Shop} />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
